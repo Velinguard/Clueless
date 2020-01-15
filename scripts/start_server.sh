278a6f1f26d2fff1a6d33559258026ece17bc13d
Sam Brotherton <sam_brotherton@yahoo.com>
2020-01-15 13:20:43 +0000

    Cleanup repo folders

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@ cd /tmp
 sudo kill -9 `sudo lsof -t -i:8080`
 
 #sudo java -jar ../target/api-0.0.1-SNAPSHOT.jar
-sudo java -jar ./target/api-0.0.1-SNAPSHOT.jar --fs=aws --email=true > log/log-`date +%F_%R`.txt 2>&1 &
+sudo java -jar ../target/api-0.0.1-SNAPSHOT.jar --fs=aws --email=true > log/log-`date +%F_%R`.txt 2>&1 &
